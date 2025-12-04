59c376af26c3dddfc4fd063371e828c4eb7e6876
Tasneem224 <tasneemtaha224@gmail.com>
2025-12-04 20:22:14 +0200

    ServiceManager

---
+++ Core/Service/ServiceManager.cs
@@ -0,0 +1,20 @@
+﻿using DomainLayer.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using ServiceAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ServiceManager(UserManager<ApplicationUser> _userManager, IConfiguration _configuration, ICloudinaryService _cloudinaryService) : IServiceManager
+    {
+        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(_userManager, _configuration, _cloudinaryService));
+
+        public IAuthenticationService AuthenticationService => _LazyAuthenticationService.Value;
+
+    }
+}
