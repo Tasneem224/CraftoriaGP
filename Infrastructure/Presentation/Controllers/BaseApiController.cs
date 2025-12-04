b7d6bd0f52575b6425ab3dda81ab440d5eef215d
Tasneem224 <tasneemtaha224@gmail.com>
2025-12-04 16:47:31 +0200

    Base Controller in Presentsion Layer

---
+++ Infrastructure/Presentation/Controllers/BaseApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        protected ActionResult SendSuccessResponse<T>(T data, string message = "Do Successfully")
+        {
+            return Ok(ApiResponse<T>.SuccessResponse(data, message));
+        }
+
+
+        protected IActionResult SendErrorResponse(string message, object errors = null, int statusCode = 400)
+        {
+            var response = ApiResponse<object>.FailResponse(message, errors);
+
+            return new ObjectResult(response)
+            {
+                StatusCode = statusCode
+            };
+        }
+
+    }
+}
---
+++ Shared/ApiResponse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string StatusCode { get; set; }
+
+        public T Data { get; set; }
+
+        // "object" هنا هو الحل السحري
+        public object Errors { get; set; }
+        public static ApiResponse<T> SuccessResponse(T data, string message = "Successful")
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                Message = message,
+                Data = data,
+                Errors = null,
+
+            };
+        }
+        public static ApiResponse<T> FailResponse(string message, object errors = null)
+        {
+            return new ApiResponse<T>
+            {
+                Success = false,
+                Message = message,
+                Data = default,
+                Errors = errors,
+            };
+        }
+    }
+}
