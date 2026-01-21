81c02bf9dd317f8ff284ddbb11d1fb615a2cf466
Tasneem224 <tasneemtaha224@gmail.com>
2026-01-21 23:14:58 +0200

    comment datasessding

--- Infrastructure/Persistance/Repositories/DataSeeding.cs
+++ Infrastructure/Persistance/Repositories/DataSeeding.cs
@@ -18,11 +18,11 @@ namespace Persistance.Repositories
             {
                 if (_roleManager.Roles.Any() is false)
                 {
-                    await _roleManager.CreateAsync(new IdentityRole("Admin"));
-                    await _roleManager.CreateAsync(new IdentityRole("Customer"));
-                    await _roleManager.CreateAsync(new IdentityRole("Supplier"));
-                    await _roleManager.CreateAsync(new IdentityRole("Expert"));
-                    await _roleManager.CreateAsync(new IdentityRole("Beginner"));
+                    //await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                    //await _roleManager.CreateAsync(new IdentityRole("Customer"));
+                    //await _roleManager.CreateAsync(new IdentityRole("Supplier"));
+                    //await _roleManager.CreateAsync(new IdentityRole("Expert"));
+                    //await _roleManager.CreateAsync(new IdentityRole("Beginner"));
                 }
                 if (_userManager.Users.Any() is false)
                 {
@@ -39,29 +39,29 @@ namespace Persistance.Repositories
 
                     //};
                     //afnan
-                    var user2 = new ApplicationUser
-                    {
-                        UserName = "AfnanAli",
-                        Email = "Afnan2@gmail.com",
-                        DisplayName = "Afnan Ali",
-                        Gender = Gender.Female,
-                        FirstName = "Afnan",
-                        SecondName = "Ali",
-                        PhoneNumber = "01099807400",
+                    //var user2 = new ApplicationUser
+                    //{
+                    //    UserName = "AfnanAli",
+                    //    Email = "Afnan2@gmail.com",
+                    //    DisplayName = "Afnan Ali",
+                    //    Gender = Gender.Female,
+                    //    FirstName = "Afnan",
+                    //    SecondName = "Ali",
+                    //    PhoneNumber = "01099807400",
 
-                    };
-                    //shiamaa
-                    var user3 = new ApplicationUser
-                    {
-                        UserName = "Shiamaa22",
-                        Email = "shiamaa925@gmail.com",
-                        DisplayName = "shiamaa Taha",
-                        Gender = Gender.Female,
-                        FirstName = "shiamaa",
-                        SecondName = "Taha",
-                        PhoneNumber = "01017830157",
+                    //};
+                    ////shiamaa
+                    //var user3 = new ApplicationUser
+                    //{
+                    //    UserName = "Shiamaa22",
+                    //    Email = "shiamaa925@gmail.com",
+                    //    DisplayName = "shiamaa Taha",
+                    //    Gender = Gender.Female,
+                    //    FirstName = "shiamaa",
+                    //    SecondName = "Taha",
+                    //    PhoneNumber = "01017830157",
 
-                    };
+                    //};
                     //mohamed taha
                     //var user4 = new ApplicationUser
                     //{
@@ -75,28 +75,28 @@ namespace Persistance.Repositories
 
                     //};
                     //gehad taha
-                    var user5 = new ApplicationUser
-                    {
-                        UserName = "gehadtaha",
-                        Email = "ghdtahagenedy@gmail.com",
-                        DisplayName = "Gehad Taha",
-                        Gender = Gender.Female,
-                        FirstName = "Gehad",
-                        SecondName = "Taha",
-                        PhoneNumber = "01033391219",
+                    //var user5 = new ApplicationUser
+                    //{
+                    //    UserName = "gehadtaha",
+                    //    Email = "ghdtahagenedy@gmail.com",
+                    //    DisplayName = "Gehad Taha",
+                    //    Gender = Gender.Female,
+                    //    FirstName = "Gehad",
+                    //    SecondName = "Taha",
+                    //    PhoneNumber = "01033391219",
 
-                    };
-                    //await _userManager.CreateAsync(user1, "P@ssw0rd");
-                    await _userManager.CreateAsync(user2, "P@ssw0rd");
-                    await _userManager.CreateAsync(user3, "P@ssw0rd");
-                    //await _userManager.CreateAsync(user4, "P@ssw0rd");
-                    await _userManager.CreateAsync(user5, "P@ssw0rd");
+                    //};
+                    ////await _userManager.CreateAsync(user1, "P@ssw0rd");
+                    //await _userManager.CreateAsync(user2, "P@ssw0rd");
+                    //await _userManager.CreateAsync(user3, "P@ssw0rd");
+                    ////await _userManager.CreateAsync(user4, "P@ssw0rd");
+                    //await _userManager.CreateAsync(user5, "P@ssw0rd");
 
-                    //await _userManager.AddToRoleAsync(user1, "Admin");
-                    await _userManager.AddToRoleAsync(user2, "Customer");
-                    await _userManager.AddToRoleAsync(user3, "Supplier");
-                    //await _userManager.AddToRoleAsync(user4, "Expert");
-                    await _userManager.AddToRoleAsync(user5, "Admin");
+                    ////await _userManager.AddToRoleAsync(user1, "Admin");
+                    //await _userManager.AddToRoleAsync(user2, "Customer");
+                    //await _userManager.AddToRoleAsync(user3, "Supplier");
+                    ////await _userManager.AddToRoleAsync(user4, "Expert");
+                    //await _userManager.AddToRoleAsync(user5, "Admin");
 
                 }
 
