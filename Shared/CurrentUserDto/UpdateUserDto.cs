3019dcecb5276be54d9520cf28d837c57892d5bb
Tasneem224 <tasneemtaha224@gmail.com>
2026-01-30 19:57:04 +0200

    create UpdateUserDto

---
+++ Shared/CurrentUserDto/UpdateUserDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.IdentityModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.CurrentUserDto
+{
+    public class UpdateUserDto
+    {
+        public string? FirstName { get; set; } = default!;
+        public string? LastName { get; set; } = default!;
+        public string? Bio { get; set; } = default!;
+        public Gender2? Gender { get; set; } = default!;
+        public string? Specialization { get; set; } = default!;
+
+        public IFormFile? ProfileImage { get; set; }
+    }
+}
