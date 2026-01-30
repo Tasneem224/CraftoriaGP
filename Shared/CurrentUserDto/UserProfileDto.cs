075cdcd7c889877328c0c7be73aacbd3ac7708d8
Tasneem224 <tasneemtaha224@gmail.com>
2026-01-30 19:56:29 +0200

    create UserProfileDto

---
+++ Shared/CurrentUserDto/UserProfileDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.CurrentUserDto
+{
+    public class UserProfileDto
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; } = default!;
+        public string SecondName { get; set; } = default!;
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+
+        public string? ProfileImage { get; set; } = default!;
+        public string? Bio { get; set; } = default!;
+        public string? Specialization { get; set; } = default!;
+    }
+}
