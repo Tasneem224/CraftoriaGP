9f48ef9334d3181b7132b25508bf97b34419bf0f
Tasneem224 <tasneemtaha224@gmail.com>
2026-01-31 00:56:21 +0200

    Add unitOfWork class

---
+++ Infrastructure/Persistance/Repositories/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public Task<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
