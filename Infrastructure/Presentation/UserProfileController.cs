8bd8b505893c6e86193ba7c5befc57649def8b2b
Tasneem224 <tasneemtaha224@gmail.com>
2026-02-01 00:42:42 +0200

    Add UserProfileController

---
+++ Infrastructure/Presentation/UserProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Controllers;
+using ServiceAbstraction;
+using Shared.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    [Authorize]
+    public class UserProfileController(IUserService _userService) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            var user = await _userService.GetCurrentUserAsync();
+            return SendSuccessResponse(user, "Data of the current User returned successfully");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateUserProfile(UpdateUserDto updateUserDto)
+        {
+            var updatedUser = await _userService.UpdateUserProfile(updateUserDto);
+            return SendSuccessResponse(updatedUser, "User Profile updated successfully");
+        }
+
+    }
+}
