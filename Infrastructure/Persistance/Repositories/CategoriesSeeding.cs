014285120711aa2e65285e029e0c43991a04f0c7
Tasneem224 <tasneemtaha224@gmail.com>
2026-01-31 02:15:34 +0200

    seeding categories data

---
+++ Infrastructure/Persistance/Repositories/CategoriesSeeding.cs
@@ -0,0 +1,40 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models.Categories;
+using DomainLayer.Models.Items;
+using Newtonsoft.Json;
+using Persistance.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class CategoriesSeeding : ICategoriesSeeding
+    {
+        private readonly StoreDbContext _storeDbContext;
+       
+        public async Task CategoryDataSeedingAsync()
+        {
+
+            try
+            {
+                string categoryJson= File.ReadAllText("categories.data.json");
+                List<Category>? categories = JsonConvert.DeserializeObject<List<Category>>(categoryJson);
+
+                if (!_storeDbContext.Categories.Any())
+                {
+                    await _storeDbContext.Categories.AddRangeAsync(categories);
+                    await _storeDbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error seeding categories: {ex.Message}");
+            }
+
+        }
+    }
+}
