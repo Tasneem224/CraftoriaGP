118e2015f758328aadb65c7b4418af6be7e2ca0e
Tasneem224 <tasneemtaha224@gmail.com>
2026-01-31 05:05:28 +0200

    Merge Identity Management

---
+++ Shared/Product/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Product
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=default!;
+        public string? ImageUrl { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+
+    }
+}
