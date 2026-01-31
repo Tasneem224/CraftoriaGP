8067f56f4327ce5181b7a7347ff77a93e4c2d264
Tasneem224 <tasneemtaha224@gmail.com>
2026-02-01 00:40:55 +0200

    Add IUserService and UserService

---
+++ Core/ServiceAbstraction/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction
+{
+    public interface IUserService
+    {
+        Task<UserProfileDto> GetCurrentUserAsync();
+        Task<UserProfileDto> UpdateUserProfile(UpdateUserDto updateUserDto);
+    }
+}
