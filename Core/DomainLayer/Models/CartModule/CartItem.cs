23208d4252b6a770a5bbbec32aa130cad438c411
Tasneem224 <tasneemtaha224@gmail.com>
2026-02-23 15:45:46 +0200

    create cart and customerITem

---
+++ Core/DomainLayer/Models/CartModule/CartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.CartModule
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PictureURL { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Qunatity { get; set; }
+    }
+}
---
+++ Core/DomainLayer/Models/CartModule/CustomerCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.CartModule
+{
+    public class CustomerCart
+    {
+        public string Id { get; set; }
+        public ICollection<CartItem> cartItems { get; set; } = [];
+    }
+}
