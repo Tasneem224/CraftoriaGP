549da3ef6f5d63fbebc93b7d992c2998989483a7
Tasneem224 <tasneemtaha224@gmail.com>
2026-02-24 17:19:28 +0200

    CacheRpository

---
+++ Core/DomainLayer/Contracts/ICacheRepository.cs
@@ -0,0 +1,16 @@
+﻿using DomainLayer.Models.CartModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface ICacheRepository
+    {
+        Task<T?> GetAsync<T>(string id);
+        Task<T?> AddOrUpdateAsync<T>(string id,T entity, TimeSpan? timeToLive = null);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ Infrastructure/Persistance/Repositories/CacheRpository.cs
@@ -0,0 +1,40 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models.CartModule;
+using Microsoft.AspNetCore.Http.HttpResults;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class CacheRpository(IConnectionMultiplexer _connection) : ICacheRepository
+    {
+
+        private readonly IDatabase _database = _connection.GetDatabase();
+        public async Task<T?> AddOrUpdateAsync<T>(string key,T entity, TimeSpan? timeToLive = null)
+        {
+            var json=JsonSerializer.Serialize(entity);
+             var created= await _database.StringSetAsync(key, json, timeToLive??TimeSpan.FromDays(3));
+            if (created)
+            {
+                return entity;
+            }
+            return default;
+        }
+
+        public async Task<bool> DeleteAsync(string id)=> await _database.KeyDeleteAsync(id);
+        
+
+        public async Task<T?> GetAsync<T>(string id)
+
+        {
+            var result = await _database.StringGetAsync(id);
+            if(result.IsNullOrEmpty) return default;
+            return JsonSerializer.Deserialize<T>(result!);
+        }
+    }
+}
