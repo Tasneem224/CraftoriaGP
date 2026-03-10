ce7d22bdabdbd3d02406e7e604a487cb0a78a4f8
Tasneem224 <tasneemtaha224@gmail.com>
2026-03-10 16:46:01 +0200

    Modify cart category by Language

--- Shared/BasketModule/CartItemDto.cs
+++ Shared/BasketModule/CartItemDto.cs
@@ -8,6 +8,7 @@ namespace Shared.BasketModule
         public string PictureURL { get; set; } = default!;
         public string ItemName { get; set; }= string.Empty;
        
+
         public string Category {  get; set; }= string.Empty;
         public decimal Price {  get; set; }
         public int CategoryId { get; set; }
