d440a8ff0240cdd0380a4c495c30b4f4b221294d
Tasneem224 <tasneemtaha224@gmail.com>
2026-04-07 07:52:14 +0200

    ChatBotMessages table

---
+++ Core/DomainLayer/Models/ChatBot/ChatBotSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.ChatBot
+{
+    public class ChatBotSession
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }        
+        public DateTime CreatedAt { get; set; }
+        public List<ChatBotMessages> Messages { get; set; } = new();
+    }
+
+}
