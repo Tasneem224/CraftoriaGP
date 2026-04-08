53906ef6a208ae9abe300021a05a986e590b602f
Tasneem224 <tasneemtaha224@gmail.com>
2026-04-08 05:55:43 +0200

    remove session table

---
+++ Core/DomainLayer/Contracts/IChatBotSessionRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainLayer.Models.ChatBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IChatBotSessionRepository
+    {
+        Task<ChatBotSession> CreateAsync(string userId);
+        Task<ChatBotSession?> GetByIdAsync(Guid sessionId);
+        Task SaveMessageAsync(ChatBotMessages message);
+        Task<List<ChatBotMessages>> GetHistoryAsync(Guid sessionId);
+
+
+    }
+
+}
