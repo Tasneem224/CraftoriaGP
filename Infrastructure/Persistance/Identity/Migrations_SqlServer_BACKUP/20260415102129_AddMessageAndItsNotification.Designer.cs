ca0206cbf9ac2c2b91dd45a2e572a520188a1514
Donia Ashraf <dynaashrf976@gmail.com>
2026-04-17 16:39:33 +0200

    commtied

--- Infrastructure/Persistance/Identity/Migrations_SqlServer_BACKUP/20260415102129_AddMessageAndItsNotification.Designer.cs
+++ Infrastructure/Persistance/Identity/Migrations_SqlServer_BACKUP/20260415102129_AddMessageAndItsNotification.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistance.Data.Contexts;
@@ -12,9 +13,11 @@ using Persistance.Data.Contexts;
 namespace Persistance.Identity.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260415102129_AddMessageAndItsNotification")]
+    partial class AddMessageAndItsNotification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
