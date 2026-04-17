57ea3b768a169cce6b377581d6bfeccae2ce203c
Tasneem224 <tasneemtaha224@gmail.com>
2026-04-17 11:04:58 +0200

    create RecommendationItemDto and MLRecommendationDto

---
+++ Shared/RecommendationSystem/MLRecommendationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.RecommendationSystem
+{
+    public class MLRecommendationDto
+    {
+        public List<RecommendationItemDto> RecommendationsItems { get; set; }
+    }
+}
---
+++ Shared/RecommendationSystem/RecommendationItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shared.RecommendationSystem
+{
+    public class RecommendationItemDto
+    {
+        [JsonPropertyName("product_id")]
+        public int ProductId { get; set; }
+
+        [JsonPropertyName("correlation_score")]
+        public double CorrelationScore { get; set; }
+    }
+}
