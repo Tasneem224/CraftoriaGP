45fe4d0f024a4ecaef1912cd83b8f32e307fb41c
Tasneem224 <tasneemtaha224@gmail.com>
2026-04-27 13:28:33 +0300

    update CommunitySpaceRepo interface

--- Core/DomainLayer/Contracts/ICommubitySpaceRepo.cs
+++ Core/DomainLayer/Contracts/ICommubitySpaceRepo.cs
@@ -9,18 +9,18 @@ namespace DomainLayer.Contracts
 {
     public interface ICommubitySpaceRepo
     {
-        Task AddPostAsync(Post post);
+        Task <Post>AddPostAsync(Post post);
         Task<Post> GetPostByIdAsync(int postId);
         Task<IEnumerable<Post>> GetAllPostsAsync();
-        Task UpdatePostAsync(Post post);
-        Task DeletePostAsync(int postId);
+        Task<Post> UpdatePostAsync(Post post);
+        Task<bool> DeletePostAsync(int postId);
         Task AddCommentAsync(Comment comment);
         Task<Comment> GetCommentByIdAsync(int commentId);
         Task<IEnumerable<Comment>> GetRepliesOnCommentByIdAsync(int commentId);
         Task<IEnumerable<Comment>> GetAllCommentsAsync(int PosrId );
-        Task UpdateCommentAsync(Comment comment);
-        Task DeleteCommentAsync(int commentId);
-        Task ToggleLoveAsync(PostLoves love);
+        Task<Post> UpdateCommentAsync(Comment comment);
+        Task<bool> DeleteCommentAsync(int commentId);
+        Task<bool> ToggleLoveAsync(PostLoves love);
         Task<int> GetCountOfLoves(int postId);
         Task<int> GetCountOfCommets(int postId);
 
---
+++ Infrastructure/Persistance/Repositories/CommunitySpaceRepo.cs
@@ -0,0 +1,84 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models.Community_Space;
+using Persistance.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    internal class CommunitySpaceRepo(StoreDbContext _context) : ICommubitySpaceRepo
+    {
+        public Task AddCommentAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> AddPostAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCommentAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePostAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Comment>> GetAllCommentsAsync(int PosrId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Post>> GetAllPostsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment> GetCommentByIdAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCountOfCommets(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCountOfLoves(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostByIdAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Comment>> GetRepliesOnCommentByIdAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ToggleLoveAsync(PostLoves love)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> UpdateCommentAsync(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> UpdatePostAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
