26350222ed415740b148c25f1a9189ed661da17a
Tasneem224 <tasneemtaha224@gmail.com>
2026-04-27 06:32:52 +0300

    create Post table

---
+++ Core/DomainLayer/Models/Community_Space/Post.cs
@@ -0,0 +1,21 @@
+﻿using DomainLayer.Models.Identity;
+using DomainLayer.Models.Items;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Community_Space
+{
+    public class Post:BaseEntity<int>
+    {
+        public string Content { get; set; }= string.Empty;
+        public string ? ImageUrl { get; set; }
+        public int AuthorId { get; set; }
+        [ForeignKey(nameof(AuthorId))]
+        public ApplicationUser Author { get; set; }
+
+    }
+}
