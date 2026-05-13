2ff7a6d486b812ee41a3d453876cde3bee0af64b
Donia Ashraf <dynaashrf976@gmail.com>
2026-05-13 22:47:19 +0300

    fixes the bug

---
+++ Infrastructure/Persistance/Identity/Migrations_SqlServer_BACKUP/20260513193742_AddIsVisibleToMessage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsVisibleToMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // هنضيف العمود يدوياً لجدول Messages
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVisible",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: true); // خليه true عشان الرسايل القديمة متختفيش
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // كود المسح في حالة الـ Rollback
+            migrationBuilder.DropColumn(
+                name: "IsVisible",
+                table: "Messages");
+        }
+    }
+}
